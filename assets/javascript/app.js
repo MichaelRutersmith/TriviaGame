104ef5b1933f90b1973a4cb678a25e5ca5bbd7cd
MichaelRutersmith <46669151+MichaelRutersmith@users.noreply.github.com>
2019-02-04 22:49:43 -0600

    Trivia Game Homework

---
+++ assets/javascript/app.js
@@ -0,0 +1,114 @@
+var rights = [];
+var wrongs = ["q1", "q2", "q3", "q4"];
+var answers = ["","Saint Paul","True","Loon","Lakes"];
+var x, seconds;
+
+
+$(document).ready(function() {
+    $("#score").hide();
+    timer();
+       
+    function review (q){
+        
+        var value = $('input[name="' + q + '"]:checked').val();
+        console.log(value);
+
+        var str = q;
+        var a = str.slice(1, 2);
+        a = parseInt(a);
+        console.log(a);
+
+        var indexRights = rights.indexOf(q);
+        if (indexRights > -1) {
+          rights.splice(indexRights, 1);
+        }
+
+        var indexWrongs = wrongs.indexOf(q);
+        if (indexWrongs > -1) {
+          wrongs.splice(indexWrongs, 1);
+        }
+     
+        if (value === answers[a]){
+            rights.push(q);
+ 
+        }else{
+             wrongs.push(q);
+   
+        };
+
+        console.log(rights);
+        console.log(wrongs);        
+    }
+
+    $("#q1").on("click", function(e){
+        review("q1");
+    });
+
+    $("#q2").on("click", function(e){
+        review("q2");
+ 
+    });
+
+    $("#q3").on("click", function(e){
+        review("q3");
+     });
+
+    $("#q4").on("click", function(e){
+        review("q4");   
+    });
+
+
+
+    $("#submit").on("click", function(e){
+
+        e.preventDefault();
+        document.getElementById("time").innerHTML = "You submitted!";
+        clearInterval(x);
+        // debugger
+
+        if (rights.length > wrongs.length) {
+                console.log(rights.length);
+                $("#questions").html('<h1 style="color: orange;"> You are a WINNER <h1>');
+                $("#score").show();
+                $("#FinalScore").html(rights.length);
+
+
+
+        } else {
+                console.log(rights.length);
+                $("#questions").html('<h1 style="color:red;"> You are a LOSER <h1>');
+                $("#score").show();
+                $("#FinalScore").html(rights.length);
+
+
+
+        }
+    });  
+});
+
+// Timer function for 90 seconds
+function timer() {
+
+        seconds = 90;    
+        x = setInterval(function()
+        {
+            document.getElementById("time").innerHTML = Math.floor(seconds/60) + " m " + seconds%60 + " s";
+           
+            seconds--;
+                       
+            if (seconds === 0) {
+                document.getElementById("time").innerHTML = "Time's Up!";
+                $("#submit").hide();
+                $("input[type=radio]").prop('disabled', true);               
+                clearInterval(x);
+            }
+
+        }, 1000);    
+}
+
+
+
+$("#restart").on("click", function restart (event)
+{
+    location.reload();
+});
